2ea5c34160854a7f7522ca4e12ac61087bdcb778
angad <angadsharma1016@gmail.com>
2018-05-10 22:08:56 +0530

    map init

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,19 +1,18 @@
-$(document).ready(()=>{
-    $("#btn").on("click",()=>{
-        var valu = $("#address").val();
-        var v="";
+$("#btn").on("click",()=>{
+    var valu = $("#address").val();
+    var v="";
 
-        for(var i in valu){
-            if(valu[i]!==" ")
-                v+=valu[i];
-            else if(valu[i]===" ")
-                v+="+";
-        }
+    for(var i in valu){
+        if(valu[i]!==" ")
+            v+=valu[i];
+        else if(valu[i]===" ")
+            v+="+";
+    }
 
-        var url = "https://maps.googleapis.com/maps/api/geocode/json?address="+v+"&key="+"AIzaSyC4g5BpF0ntigP5d3LfulYDUUT0bxWvC54";
-        $.get(url,(data)=>{
-            var res = data.results[0].geometry.location;
-            console.log(res);
-        });
+    var url = "https://maps.googleapis.com/maps/api/geocode/json?address="+v+"&key="+"AIzaSyC4g5BpF0ntigP5d3LfulYDUUT0bxWvC54";
+    $.get(url,(data)=>{
+        var res = data.results[0].geometry.location;
+        console.log(res);
+
     });
 });
